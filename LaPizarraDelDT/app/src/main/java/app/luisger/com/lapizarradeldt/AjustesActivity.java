fbc3e55d12a6c703cd810bfc11c4b3e2c452aefa
Unknown <puddle.n@gmail.com>
2017-12-04 14:26:51 -0500

    Day/Night Mode Add

--- LaPizarraDelDT/app/src/main/java/app/luisger/com/lapizarradeldt/AjustesActivity.java
+++ LaPizarraDelDT/app/src/main/java/app/luisger/com/lapizarradeldt/AjustesActivity.java
@@ -1,5 +1,6 @@
 package app.luisger.com.lapizarradeldt;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,7 +8,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mahfa.dnswitch.DayNightSwitch;
+import com.mahfa.dnswitch.DayNightSwitchListener;
 
 /**
  * Created by Luisger94 on 8/4/2017.
@@ -15,19 +21,43 @@ import android.widget.TextView;
 
 public class AjustesActivity extends AppCompatActivity {
 
+    DayNightSwitch dayNightSwitch;
+    View background_view;
+    TextView text;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ajustes_layout);
-
-        TextView title = (TextView) findViewById(R.id.ajustesActivityTitle1);
-        title.setText("Ajustes Activity");
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(3);
         menuItem.setChecked(true);
+
+        dayNightSwitch = (DayNightSwitch)findViewById(R.id.dayNight);
+        background_view = findViewById(R.id.background_view);
+        text = (TextView) findViewById(R.id.textView);
+
+        dayNightSwitch.setDuration(450);
+        dayNightSwitch.setListener(new DayNightSwitchListener() {
+            @Override
+            public void onSwitch(boolean isNight) {
+                if(isNight)
+                {
+                    Toast.makeText(AjustesActivity.this, "Night Mode Selected", Toast.LENGTH_SHORT).show();
+                    background_view.setAlpha(1f);
+                    text.setTextColor(Color.WHITE);
+                }
+                else
+                {
+                    Toast.makeText(AjustesActivity.this, "Day Mode Selected", Toast.LENGTH_SHORT).show();
+                    background_view.setAlpha(0f);
+                    text.setTextColor(Color.BLACK);
+                }
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
             @Override
